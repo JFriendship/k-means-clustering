608223c63a6a7c30e5c797eecd700aa346bcc625
JFriendship <jackfriendshipusa@gmail.com>
2025-07-06 20:53:28 -0700

    Add Image Segmentation Flask Display

--- Image-Segmentation/app.py
+++ Image-Segmentation/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template, url_for
 from werkzeug.utils import secure_filename
 import os
+from image_segmentation import segment_image
 
 app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+PROCESS_FOLDER = os.path.join('static', 'processed')
+app.config['PROCESS_FOLDER'] = PROCESS_FOLDER
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -16,17 +20,23 @@ def allowed_file(filename):
 def upload_image():
     filename = None
     error = None
+    segmented = None
     if request.method == 'POST':
         file = request.files['image']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
+
+            seg_filepath = os.path.join(app.config['PROCESS_FOLDER'], filename)
+            k = request.form.get('k')
+            print("Obtained value k: ", k)
+            segmented = segment_image(file_name=filename, k=int(k))
         else:
             error = 'File type not allowed. Please upload a .jpg, .jpeg, or .png file.'
-        k = request.form.get('k')
-        print("Obtained value k: ", k)
-    return render_template('upload.html', filename=filename, error=error)
+        
+
+    return render_template('upload.html', filename=filename, segmented=segmented, error=error)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
