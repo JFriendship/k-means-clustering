8480b64bc4f954bce7b054377b7d99ff26ccd14c
JFriendship <jackfriendshipusa@gmail.com>
2025-07-16 11:56:14 -0700

    Add Dockerfile and Update app host

--- Image-Segmentation/app.py
+++ Image-Segmentation/app.py
@@ -43,4 +43,4 @@ def uploaded_file(filename):
     return url_for('static', filename='uploads/' + filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=3000)
